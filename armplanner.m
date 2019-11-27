1a7af3776d502ad88d82ffd74cd20c462df8fb7a
Rohan Tiwari <rohan@Rohans-MBP-3.wv.cc.cmu.edu>
2019-11-27 16:18:51 -0500

    Removed Redundant Files

--- armplanner.m
+++
@@ -1,4 +0,0 @@
-function[armplan, armplanlength,time,cost,vertices] = armplanner(envmap, armstart, armgoal, planner_id)
-%call the planner in C
-[armplan, armplanlength,time,cost,vertices] = planner(envmap, armstart, armgoal, planner_id);
-
